614da18b0889fb0caf7f3130244b44a79db69862
PanzerWeb <selwynvillar@gmail.com>
2024-05-31 17:21:14 +0800

    Finish

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+
+// Get the navigation bar
+const offHeader = document.getElementById('offcanvasNavbar');
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', function() {
+    // Check if the user has scrolled down past a certain point (e.g. 10px)
+    if (window.scrollY > 10) {
+        // Add the 'scrolled' class to the navbar
+        offHeader.classList.add('scrolled');
+    } else {
+        // Remove the 'scrolled' class from the navbar
+        offHeader.classList.remove('scrolled');
+    }
+});
